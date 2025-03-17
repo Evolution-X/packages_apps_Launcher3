242b7c652cfe636c82a2f5aa12322bbc140021b1
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-17 17:03:13 +0800

    Expose split overview action button
    Change-Id: I37623b6dee4774edb6f1326a19758e5d7bb54643

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -273,8 +273,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private void updateForIsTablet() {
         assert mDp != null;
         // Update flags to see if split button should be hidden.
-        updateSplitButtonHiddenFlags(FLAG_SMALL_SCREEN_HIDE_SPLIT, !mDp.isTablet ||
-                getContext().getSystemService(ActivityManager.class).isLowRamDevice());
+        updateSplitButtonHiddenFlags(FLAG_SMALL_SCREEN_HIDE_SPLIT, false);
         updateActionButtonsVisibility();
     }
 
